1f54d5efcf8ddbd727c58c4fd03743efc7649d33
JP-Mendonca <joaoooh@gmail.com>
2021-03-20 22:00:35 -0300

    Arrumando espaços

--- Devices.cs
+++ Devices.cs
@@ -8,43 +8,35 @@ namespace devices
         String GetIpAddr();
         String GetDefGat();
     }
-
     class Device : IDevice 
     {
         private String Name { get; set; }
         private String ip_addr { get; set; }
         private String default_gateway { get; set; }
-
         public Device(String _name, String _ip_addr, String def_gat)
         {
             Name = _name;
             ip_addr = _ip_addr;
             default_gateway = def_gat;
         }
-
         public String GetName()
         {
             return Name;
         }
-
         public String GetIpAddr()
         {
             return ip_addr;
         }
-
         public String GetDefGat()
         {
             return default_gateway;
         }
-
         public virtual String GetAttributes()
         {
             String[] attributes = { Name, ip_addr, default_gateway };
             return String.Join(", ", attributes);
         }
-
     }
-
     class iPhoneAdapter : Device
     {
         private iPhone my_iPhone = new iPhone();
@@ -54,16 +46,13 @@ namespace devices
         {
             return String.Join(", ", base.GetAttributes()+", "+my_iPhone.typeOfDevice);
         }
-
     }
-
     class iPhone
     {
         public String typeOfDevice = "This device is an iPhone";
         public iPhone()
         {}
     }
-
     class AndroidAdapter : Device
     {
         private Android my_android = new Android();
@@ -74,12 +63,10 @@ namespace devices
             return String.Join(", ", base.GetAttributes()+", "+my_android.typeOfDevice);
         }
     }
-
     class Android
     {
         public String typeOfDevice = "This device is an android";
         public Android()
         {}
     }
-
 }
